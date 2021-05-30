0ac44216df69e0ee8a06d2293f37ab1983dc224f
Arturo Iván Chávez Gómez <l17120232@morelia.tecnm.mx>
2021-05-30 18:34:15 -0500

    first commit

---
+++ resources/views/components/nav-link.blade.php
@@ -0,0 +1,11 @@
+@props(['active'])
+
+@php
+$classes = ($active ?? false)
+            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-black text-md font-medium leading-5 text-black focus:outline-none focus:border-blue-700 transition duration-150 ease-in-out'
+            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-md font-medium leading-5 text-gray-700 hover:text-gray-800 hover:border-gray-500 focus:outline-none focus:text-gray-700 focus:border-gray-500 transition duration-150 ease-in-out';
+@endphp
+
+<a {{ $attributes->merge(['class' => $classes]) }}>
+    {{ $slot }}
+</a>
